dcfdb61d19c2ce8d5653e9a058b13d81b3608777
unknown <mauricio.fernandez.l@gmx.de>
2020-05-09 07:32:04 +0200

    update with selective interface

--- scripts/main.py
+++ scripts/main.py
@@ -15,7 +15,7 @@ def bR(c1s,c2s,f2):
 
 #%% Plot
 
-def plot(c1s,c2s,orientation='v'):
+def plot(c1s,c2s,orientation='h'):
     f2s = np.linspace(0,1,20)
     
     p = np.array([[f(c1s,c2s,f2) for f2 in f2s] for f in [bV,bR]])
@@ -45,33 +45,58 @@ plot(c1s,c2s)
 
 #%% Interactive plot
 
-# Variables
+# Interactive variables
 c11 = widgets.IntSlider(1,min=1,max=10,description='$c^{(1)}_1 = 3K^{(1)}$')
 c12 = widgets.IntSlider(1,min=1,max=10,description='$c^{(1)}_2 = 2G^{(1)}$')
 c21 = widgets.IntSlider(10,min=1,max=10,description='$c^{(2)}_1 = 3K^{(2)}$')
 c22 = widgets.IntSlider(10,min=1,max=10,description='$c^{(2)}_2 = 2G^{(2)}$')
 
-# Function
-def plot2(c11,c12,c21,c22):
-    plot(np.array([c11,c12]),np.array([c21,c22]))
+#%% Selective interface
 
-# Bottom
-bottom_manual = widgets.interactive(
-    plot2
-    ,{'manual':True}
-    ,c11=c11,c12=c12,c21=c21,c22=c22
+devices = widgets.Dropdown(
+    options = [('',0),('computer',1),('smartphone',2)]
+    ,description = 'Select device'
 )
-bottom_manual.children[-2].description = 'Run/Update'
 
-def start(device='smartphone'):
-    print('Starting interactive plots')
-    if device=='smartphone':
-        head = widgets.VBox([widgets.Label('Material 1'),c11,c12,widgets.Label('Material 2'),c21,c22])
-        display(head)
-    else: 
+def plot_computer(c11,c12,c21,c22):
+    plot(np.array([c11,c12]),np.array([c21,c22]))
+    
+def plot_smartphone(c11,c12,c21,c22):
+    plot(np.array([c11,c12]),np.array([c21,c22]),orientation='v')
+
+def check(device):
+    if device==0:
+        print('Please choose a device.')
+    if device==1:
+        print('You are using a computer')
         head1 = widgets.HBox([widgets.Label('Material 1'),c11,c12])
         head2 = widgets.HBox([widgets.Label('Material 2'),c21,c22])
+        bottom = widgets.interactive(
+            plot_computer
+            ,{'manual':True}
+            ,c11=c11,c12=c12,c21=c21,c22=c22
+        )
+        bottom.children[-2].description = 'Run/Update'
         display(head1)
         display(head2)
-    display(bottom_manual.children[-2])
-    display(bottom_manual.children[-1])
+        display(bottom.children[-2])
+        display(bottom.children[-1])
+    if device==2:
+        head = widgets.VBox([widgets.Label('Material 1'),c11,c12,widgets.Label('Material 2'),c21,c22])
+        bottom = widgets.interactive(
+            plot_smartphone
+            ,{'manual':True}
+            ,c11=c11,c12=c12,c21=c21,c22=c22
+        )
+        bottom.children[-2].description = 'Run/Update'
+        display(head)
+        display(bottom.children[-2])
+        display(bottom.children[-1])
+        
+check_w = widgets.interactive(
+    check
+    ,device=devices
+)
+    
+def start_selective():
+    display(check_w)
