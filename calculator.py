1ef4838ce52555efb3a64b31c9ecce925a812b7e
Mauricio <mauricio.fernandez.l@gmx.de>
2020-05-25 13:36:36 +0200

    update with new functions and extended tests

--- calculator.py
+++ calculator.py
@@ -9,3 +9,11 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(a, b):
+    return a*b
+
+
+def divide(a, b):
+    return a/b
--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 10 == calculator.multiply(2, 5)
+
+    def test_division(self):
+        assert 7 == calculator.divide(21, 3)
